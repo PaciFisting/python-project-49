401959c9bac7057f9511b81d833fabdd1b383e84
PaciFisting <lex.n.99@bk.ru>
2025-05-28 18:12:49 +0500

    brain_calc

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,7 +6,7 @@ def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    correct_anwers = 0
+    correct_answers = 0
     for round in range(0,3):
         number = random.randint(1, 100)
         print(f'Question: {number}')
@@ -14,16 +14,16 @@ def main():
         if number % 2 == 0: 
             if answer.lower() == 'yes':
                 print('Correct!')
-                correct_anwers += 1
+                correct_answers += 1
             else: 
                 print (f'{answer} is wrong answer ;(. Correct answer was "yes".\nLet\'s try again, {name}!')
                 break
         if number % 2 != 0: 
             if answer.lower() == 'no':
                 print('Correct!')
-                correct_anwers += 1
+                correct_answers += 1
             else: 
                 print (f'{answer} is wrong answer ;(. Correct answer was "no".\nLet\'s try again, {name}!')
                 break
-    if correct_anwers == 3:
+    if correct_answers == 3:
         print(f'Congratulations, {name}!')
