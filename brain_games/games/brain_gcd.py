b1f95f355e995507aa1fa38a8db873345d3f608d
PaciFisting <lex.n.99@bk.ru>
2025-06-04 13:54:26 +0500

    fix_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -6,7 +6,7 @@ from brain_games import engine
 def round_generator():
     number_1 = random.randint(1, 100)
     number_2 = random.randint(1, 100)
-    question = f'{number_1}, {number_2}'
+    question = f'{number_1} {number_2}'
     while number_2 != 0:
         number_1, number_2 = number_2, number_1 % number_2
         if number_2 == 0:
