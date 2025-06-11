de46b7a6530bec15c7e2078c27aebfffde97caf9
PaciFisting <lex.n.99@bk.ru>
2025-06-11 19:50:45 +0500

    fixes

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -11,19 +11,24 @@ def calculate_expression(number_1, operation, number_2):
             return number_1 - number_2
         case '*':
             return number_1 * number_2
+        
 
-
-def generate_round():
-    number_1 = random.randint(0, 100)
-    operations = ['+', '-', '*']
-    random_operation = random.choice(operations)
-    match random_operation:
+def create_numbers(operation):
+    number_1 = random.randint(1, 100)
+    match operation:
         case '+':
             number_2 = random.randint(0, 100)
         case '-':
             number_2 = random.randint(0, number_1)
         case '*':
             number_2 = random.randint(0, 10)
+    return number_1, number_2
+
+
+def generate_round():
+    operations = ['+', '-', '*']
+    random_operation = random.choice(operations)
+    number_1, number_2 = create_numbers(random_operation)
     correct_answer = calculate_expression(number_1, random_operation, number_2)
     question = f'{number_1} {random_operation} {number_2}'
     return question, correct_answer
